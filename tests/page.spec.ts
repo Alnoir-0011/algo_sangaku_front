6f3abf961e203fdc18f4ba35f086b8d1250dedfd
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-02-17 15:50:17 +0900

    テストがドロワーを無視するように修正

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -10,8 +10,9 @@ test.describe('TopPage', () => {
   });
 
   test('has description', async ({ page }) => {
+    const mainNode = page.locator('main');
     await expect(
-      page.getByText(
+      mainNode.getByText(
         '算額とは、神社や寺院に奉納された和算の絵馬のことで、日本独自に広まった文化だと言われています。難問が多いですが、問題が解けた喜びを神仏に感謝したり、学業成就を祈願する風習として親しまれてきました。',
         { exact: true }
       )
@@ -19,22 +20,26 @@ test.describe('TopPage', () => {
   })
 
   test('has link to createSangaku page', async ({ page }) => {
-    const link = page.getByRole('link', { name: '算額を作る' });
+    const mainNode = page.locator('main');
+    const link = mainNode.getByRole('link', { name: '算額を作る' });
     await expect(link).toBeVisible();
   });
 
   test('has link to showSangakus page', async ({ page }) => {
-    const link = page.getByRole('link', { name: '算額を確認' });
+    const mainNode = page.locator('main');
+    const link = mainNode.getByRole('link', { name: '算額を確認' });
     await expect(link).toBeVisible();
   });
 
   test('has link to map page', async ({ page }) => {
-    const link = page.getByRole('link', { name: '神社を探す' });
+    const mainNode = page.locator('main');
+    const link = mainNode.getByRole('link', { name: '神社を探す' });
     await expect(link).toBeVisible();
   });
 
   test('has link to answerSangaku page', async ({ page }) => {
-    const link = page.getByRole('link', { name: '算額を解く' });
+    const mainNode = page.locator('main');
+    const link = mainNode.getByRole('link', { name: '算額を解く' });
     await expect(link).toBeVisible();
   });
 });
