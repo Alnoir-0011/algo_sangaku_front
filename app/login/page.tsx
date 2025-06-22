1827b144626e90bc47c2f94926a1bc3b5b526fb0
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-06-22 16:26:04 +0900

    テスト用のcredentialsログインを追加

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
-import { signIn } from "@/auth";
+import { redirect } from "next/navigation";
+import { providerMap, signIn, auth } from "@/auth";
 import { Box, Button, Paper, Typography } from "@mui/material";
+import { AuthError } from "next-auth";
 import GoogleIcon from "@mui/icons-material/Google";
+import TextField from "@mui/material/TextField";
 
-export default function Login() {
+const SIGNIN_ERROR_URL = "/error";
+
+interface Props {
+  searchParams: { callbackUrl: string | undefined };
+}
+
+export default function Login({ searchParams }: Props) {
   return (
     <Box
       sx={{
@@ -16,17 +25,73 @@ export default function Login() {
         <Typography variant="h5" sx={{ mb: 2 }}>
           ログイン
         </Typography>
-        <form
-          action={async () => {
-            "use server";
-            await signIn("google");
-          }}
-        >
-          <Button variant="contained" type="submit" sx={{ bgcolor: "#4285F4" }}>
-            <GoogleIcon sx={{ mr: 1 }} />
-            Googleでログイン
-          </Button>
-        </form>
+        {process.env.APP_ENV === "test" && (
+          <Box sx={{ mb: 3 }}>
+            <form
+              action={async (formData) => {
+                "use server";
+                try {
+                  await signIn("credentials", formData);
+                } catch (error) {
+                  if (error instanceof AuthError) {
+                    return redirect(`${SIGNIN_ERROR_URL}?.error=${error.type}`);
+                  }
+                  throw error;
+                }
+              }}
+            >
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="email"
+                variant="standard"
+              />
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="password"
+                variant="standard"
+                sx={{ mb: 2 }}
+              />
+              <div style={{ display: "flex", justifyContent: "end" }}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  sx={{ mr: 0, ml: "auto" }}
+                >
+                  サインイン
+                </Button>
+              </div>
+            </form>
+          </Box>
+        )}
+        {Object.values(providerMap).map((provider) => (
+          <form
+            key={provider.id}
+            action={async () => {
+              "use server";
+              try {
+                await signIn(provider.id, {
+                  redirectTo: searchParams?.callbackUrl ?? "",
+                });
+              } catch (error) {
+                if (error instanceof AuthError) {
+                  return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
+                }
+                throw error;
+              }
+            }}
+          >
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ bgcolor: "#4285F4" }}
+            >
+              {provider.id === "google" && <GoogleIcon sx={{ mr: 1 }} />}
+              <span>{provider.name}でログイン</span>
+            </Button>
+          </form>
+        ))}
       </Paper>
     </Box>
   );
--- auth.ts
+++ auth.ts
@@ -1,32 +1,64 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
 import Google from "next-auth/providers/google";
-import { JWTDecodeParams, JWTEncodeParams } from "next-auth/jwt";
-import type { JWT } from "next-auth/jwt";
+import type { Provider } from "next-auth/providers";
+// テスト用
+import Credentials from "next-auth/providers/credentials";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const providers = [Google];
+const providers: Provider[] = [Google];
 
-const jwtTestEnv = {
-  async encode(params: JWTEncodeParams<JWT>): Promise<string> {
-    return btoa(JSON.stringify(params.token));
-  },
-  async decode(params: JWTDecodeParams): Promise<JWT | null> {
-    if (!params.token) return {};
-    return JSON.parse(atob(params.token));
-  },
-};
+if (process.env.APP_ENV === "test") {
+  providers.push(
+    Credentials({
+      id: "password",
+      name: "Password",
+      credentials: {
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        console.log(credentials);
+        if (credentials.password === "password") {
+          const user: User = {
+            email: "test_user@example.com",
+            name: "Test user",
+            image: "https://avatars.githubusercontent.com/u/67470890?s=200&v=4",
+          };
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  );
+}
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
-  ...(process.env.APP_ENV === "test" ? { jwt: jwtTestEnv } : {}),
   providers,
   callbacks: {
     async signIn({ account, user }) {
-      const idToken = account?.id_token;
+      // テスト用
+      if (process.env.APP_ENV === "test") {
+        user.nickname = "test_nickname";
+        user.accessToken = "dummy_token";
+        return true;
+      }
 
+      const idToken = account?.id_token;
       try {
         const response = await fetch(`${apiUrl}/api/v1/users`, {
           method: "POST",
